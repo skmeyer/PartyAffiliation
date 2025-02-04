4f632d27f463f93860e1e4149d38af70e6a40775
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-04 18:06:03 -0500

    Initial commit

---
+++ src/PartyAffiliation.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PartyAffiliation
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String partyAffiliation = "";
+
+        System.out.println("Enter your political party affiliation with the corresponding first letter [DRIO]: ");
+    }
+}
