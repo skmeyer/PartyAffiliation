d8a109f0c848f44742546ba5a26d70803b9eefb5
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-04 18:33:56 -0500

    Complete

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -7,6 +7,23 @@ public class PartyAffiliation
         Scanner in = new Scanner(System.in);
         String partyAffiliation = "";
 
-        System.out.println("Enter your political party affiliation with the corresponding first letter [DRIO]: ");
+        System.out.print("Enter your party affiliation (Democrat, Republican, or Independent) with the corresponding first letter: ");
+        partyAffiliation = in.nextLine();
+
+        if (partyAffiliation.equalsIgnoreCase("D"))
+        {
+            System.out.println("You get a Democratic Donkey!");
+        }
+        else if(partyAffiliation.equalsIgnoreCase("R"))
+        {
+            System.out.println("You get a Republican Elephant!");
+        }
+        else if(partyAffiliation.equalsIgnoreCase("I"))
+        {
+            System.out.println("You get an Independent Man!");
+        }
+        else
+            System.out.println("Your party is Other, and not listed here.");
+
     }
 }
